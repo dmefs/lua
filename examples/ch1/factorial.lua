58824a41f3a55ac9c8dd5733c2dea49ce8922263
dmefs <dennis.mefs@gmail.com>
2023-07-27 17:01:08 +0800

    Initial update

---
+++ examples/ch1/factorial.lua
@@ -0,0 +1,11 @@
+-- defines a factoral function
+function fact (n)
+    if n == 0 then
+        return 1
+    else
+        return n * fact(n-1)
+    end
+end
+
+a = io.read("number")
+print(fact(a))
